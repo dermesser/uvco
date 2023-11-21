992914467515d500267bfbe06c8e86224b9767e4
Lewin Bormann <lbo@spheniscida.de>
2023-11-21 13:17:23 +0100

    More Ticker unit tests

--- test/timer_test.cc
+++ test/timer_test.cc
@@ -24,7 +24,9 @@ TEST(TimerTest, tickerTest) {
     auto ticker = uvco::tick(loop, 10, count);
     uvco::MultiPromise<uint64_t> tickerProm = ticker->ticker();
     while (true) {
-      if (co_await tickerProm) {
+      std::optional<uint64_t> got = co_await tickerProm;
+      if (got) {
+        EXPECT_EQ(*got, counter);
         ++counter;
       } else {
         break;
@@ -34,4 +36,37 @@ TEST(TimerTest, tickerTest) {
 
   run_loop(setup);
   EXPECT_EQ(counter, count);
+}
+
+TEST(TimerTest, infiniteTickerTest) {
+  constexpr static uint64_t count = 3;
+  uint64_t counter = 0;
+  auto setup = [&](uv_loop_t *loop) -> uvco::Promise<void> {
+    auto ticker = uvco::tick(loop, 10, 0);
+    uvco::MultiPromise<uint64_t> tickerProm = ticker->ticker();
+    for (counter = 0; counter < count; ++counter) {
+      EXPECT_EQ(counter, *(co_await tickerProm));
+    }
+    co_await ticker->close();
+  };
+
+  run_loop(setup);
+  EXPECT_EQ(counter, count);
+}
+
+TEST(TimerTest, finiteTickerTest) {
+  constexpr static uint64_t count = 30;
+  constexpr static uint64_t stopAfter = 3;
+  uint64_t counter = 0;
+  auto setup = [&](uv_loop_t *loop) -> uvco::Promise<void> {
+    auto ticker = uvco::tick(loop, 10, count);
+    uvco::MultiPromise<uint64_t> tickerProm = ticker->ticker();
+    for (counter = 0; counter < stopAfter; ++counter) {
+      EXPECT_EQ(counter, *(co_await tickerProm));
+    }
+    co_await ticker->close();
+  };
+
+  run_loop(setup);
+  EXPECT_EQ(counter, stopAfter);
 }
