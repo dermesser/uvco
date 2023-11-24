eee72390b6f06e2f277c06703ae1effe947af6ca
Lewin Bormann <lbo@spheniscida.de>
2023-11-24 19:01:26 +0100

    Implement bounded channels (like go)

--- src/channel.h
+++ src/channel.h
@@ -12,27 +12,27 @@ public:
   explicit BoundedQueue(size_t capacity) { queue_.reserve(capacity); }
 
   template <typename U> void push(U &&elem) {
-    BOOST_ASSERT(size() < queue_.capacity());
-    if (queue_.size() < queue_.capacity()) {
+    BOOST_ASSERT(hasSpace());
+    if (queue_.size() < capacity()) {
       BOOST_ASSERT(tail_ <= head_);
       queue_.push_back(std::forward<U>(elem));
     } else {
       queue_.at(head_) = std::forward<U>(elem);
     }
-    head_ = (head_ + 1) % queue_.capacity();
+    head_ = (head_ + 1) % capacity();
     ++size_;
   }
   T pop() {
     BOOST_ASSERT(size() > 0);
     T t = std::move(queue_.at(tail_++));
-    tail_ = tail_ % queue_.capacity();
+    tail_ = tail_ % capacity();
     --size_;
     return t;
   }
-  size_t size() const {
-    return size_;
-  }
+  size_t size() const { return size_; }
+  size_t capacity() const { return queue_.capacity(); }
   bool empty() const { return size() == 0; }
+  bool hasSpace() const { return size() < capacity(); }
 
 private:
   std::vector<T> queue_{};
@@ -44,11 +44,79 @@ private:
 };
 
 // A bounded-capacity channel for items of type 'T'.
+// Can only be written to or read from by one coroutine at a time; more than one
+// coroutine waiting is forbidden.
+//
+// A reader waits while the channel is empty, and is awoken by the first writer.
+// A writer waits while the channel is full, and is awoken by the first reader.
 template <typename T> class Channel {
 public:
-  explicit Channel(size_t capacity) {}
+  explicit Channel(size_t capacity) : queue_{capacity} {}
+
+  template <typename U> Promise<void> put(U &&value) {
+    if (!queue_.hasSpace()) {
+      // Block until a reader has popped an item.
+      ChannelAwaiter_ awaiter{queue_, write_waiting_};
+      BOOST_ASSERT(co_await awaiter);
+    }
+    queue_.push(std::forward<U>(value));
+
+    // NOTE: this will switch control to the reader until it suspends; keep this
+    // in mind.
+    //
+    // For a filled queue, this will result in a nice lock-step switching back
+    // and forth.
+    awake_reader();
+    co_return;
+  }
+
+  Promise<T> get() {
+    if (queue_.empty()) {
+      ChannelAwaiter_ awaiter{queue_, read_waiting_};
+      BOOST_ASSERT(co_await awaiter);
+    }
+    T item{queue_.pop()};
+    // NOTE: this will switch control to the writer until it suspends; keep this
+    // in mind.
+    awake_writer();
+    co_return item;
+  }
 
 private:
+  void awake_reader() {
+    if (read_waiting_) {
+      auto resume = *read_waiting_;
+      read_waiting_.reset();
+      resume.resume();
+    }
+  }
+  void awake_writer() {
+    if (write_waiting_) {
+      auto resume = *write_waiting_;
+      write_waiting_.reset();
+      resume.resume();
+    }
+  }
+
+  BoundedQueue<T> queue_;
+  std::optional<std::coroutine_handle<>> read_waiting_;
+  std::optional<std::coroutine_handle<>> write_waiting_;
+
+  struct ChannelAwaiter_ {
+    explicit ChannelAwaiter_(BoundedQueue<T> &queue,
+                             std::optional<std::coroutine_handle<>> &slot)
+        : queue_{queue}, slot_{slot} {}
+    bool await_ready() { return false; }
+    bool await_suspend(std::coroutine_handle<> handle) {
+      BOOST_ASSERT(!slot_);
+      slot_ = handle;
+      return true;
+    }
+    bool await_resume() { return !queue_.empty(); }
+
+    BoundedQueue<T> &queue_;
+    std::optional<std::coroutine_handle<>> &slot_;
+  };
 };
 
 } // namespace uvco
--- test/channel_test.cc
+++ test/channel_test.cc
@@ -1,5 +1,6 @@
 
 #include "channel.h"
+#include "test_util.h"
 
 #include <gtest/gtest.h>
 
@@ -21,7 +22,7 @@ TEST(BQTest, basicPushPop) {
 
   bq.push(3);
   bq.push(4);
-  
+
   EXPECT_EQ(bq.size(), 3);
   EXPECT_EQ(bq.pop(), 2);
 
@@ -32,7 +33,7 @@ TEST(BQTest, basicPushPop) {
   EXPECT_EQ(bq.pop(), 3);
 
   bq.push(7);
-  
+
   EXPECT_EQ(bq.size(), 4);
   EXPECT_EQ(bq.pop(), 4);
 }
@@ -41,7 +42,7 @@ TEST(BQTest, pushTooMany) {
   BoundedQueue<int> bq(2);
   bq.push(1);
   bq.push(2);
-  EXPECT_DEATH({ bq.push(3); }, "size\\(\\) < queue_.capacity");
+  EXPECT_DEATH({ bq.push(3); }, "hasSpace");
 }
 
 TEST(BQTest, popEmpty) {
@@ -52,4 +53,62 @@ TEST(BQTest, popEmpty) {
   EXPECT_EQ(bq.pop(), 2);
   EXPECT_EQ(bq.size(), 0);
   EXPECT_DEATH({ bq.pop(); }, "size\\(\\) > 0");
+}
+
+TEST(ChannelTest, basicWriteRead) {
+
+  auto setup = [&](uv_loop_t *) -> Promise<void> {
+    Channel<int> ch{3};
+
+    co_await ch.put(1);
+    co_await ch.put(2);
+    EXPECT_EQ(co_await ch.get(), 1);
+  };
+
+  run_loop(setup);
+}
+
+TEST(ChannelTest, blockingRead) {
+
+  auto drain = [](Channel<int> &ch) -> Promise<void> {
+    for (int i = 1; i < 3; ++i) {
+      EXPECT_EQ(co_await ch.get(), i);
+    }
+  };
+  auto setup = [&](uv_loop_t *) -> Promise<void> {
+    Channel<int> ch{3};
+
+    Promise<void> drainer = drain(ch);
+
+    co_await ch.put(1);
+    co_await ch.put(2);
+    co_await ch.put(3);
+    co_await ch.put(4);
+
+    EXPECT_EQ(co_await ch.get(), 3);
+    EXPECT_EQ(co_await ch.get(), 4);
+  };
+
+  run_loop(setup);
+}
+
+TEST(ChannelTest, blockingWrite) {
+
+  auto source = [](Channel<int> &ch, int n) -> Promise<void> {
+    for (int i = 1; i < n + 1; ++i) {
+      co_await ch.put(i);
+    }
+  };
+  auto setup = [&](uv_loop_t *) -> Promise<void> {
+    Channel<int> ch{2};
+
+    Promise<void> sourcer = source(ch, 4);
+
+    EXPECT_EQ(co_await ch.get(), 1);
+    EXPECT_EQ(co_await ch.get(), 2);
+    EXPECT_EQ(co_await ch.get(), 3);
+    EXPECT_EQ(co_await ch.get(), 4);
+  };
+
+  run_loop(setup);
 }
