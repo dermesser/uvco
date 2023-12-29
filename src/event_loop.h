0a2ee6c898b9604db50bafa062622eea0ec00260
Lewin Bormann <lbo@spheniscida.de>
2023-12-29 11:40:31 +0100

    Remove obsolete event_loop library

--- src/event_loop.h
+++
@@ -1,9 +0,0 @@
-// uvco (c) 2023 Lewin Bormann. See LICENSE for specific terms.
-
-#pragma once
-
-namespace uvco {
-
-void run_loop();
-
-} // namespace uvco
--- src/test_event_loop.cc
+++
@@ -1,9 +0,0 @@
-// uvco (c) 2023 Lewin Bormann. See LICENSE for specific terms.
-
-#include "event_loop.h"
-
-int main() {
-  uvco::run_loop();
-
-  return 0;
-}
