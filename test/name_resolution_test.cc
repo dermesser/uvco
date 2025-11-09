58ac10c038be442c58b624be68e586d6411113c3
Lewin Bormann <lbo@spheniscida.de>
2025-11-09 20:41:58 +0100

    Improve test coverage for name resolution code

--- test/name_resolution_test.cc
+++ test/name_resolution_test.cc
@@ -53,13 +53,49 @@ TEST(NameResolutionTest, ipv6Parse) {
   EXPECT_EQ(ah.family(), AF_INET6);
 }
 
+TEST(NameResolutionTest, sockaddr) {
+  AddressHandle ah4{"127.0.0.1", 1234};
+  const struct sockaddr *sa4 = ah4.sockaddr();
+  EXPECT_EQ(sa4->sa_family, AF_INET);
+  const struct sockaddr_in *sin4 = (const struct sockaddr_in *)sa4;
+  EXPECT_EQ(ntohl(sin4->sin_addr.s_addr), 0x7f000001);
+  EXPECT_EQ(ntohs(sin4->sin_port), 1234);
+}
+
+TEST(NameResolutionTest, fromSockaddr) {
+  AddressHandle ah4{"127.0.0.1", 1234};
+  AddressHandle ah6{"::1", 1234};
+
+  const struct sockaddr *sa4 = ah4.sockaddr();
+  EXPECT_EQ(sa4->sa_family, AF_INET);
+
+  const struct sockaddr *sa6 = ah6.sockaddr();
+  EXPECT_EQ(sa6->sa_family, AF_INET6);
+
+  AddressHandle ah4_2{sa4};
+  EXPECT_EQ(ah4.toString(), "127.0.0.1:1234");
+  EXPECT_EQ(ah4_2.toString(), "127.0.0.1:1234");
+
+  AddressHandle ah6_2{sa6};
+  EXPECT_EQ(ah6.toString(), "[::1]:1234");
+  EXPECT_EQ(ah6_2.toString(), "[::1]:1234");
+
+  struct sockaddr saFake = *sa6;
+  saFake.sa_family = 1234;
+  EXPECT_THROW({ AddressHandle ahInvalid{&saFake}; }, UvcoException);
+}
+
 TEST(NameResolutionTest, resolveGoogleDns) {
   auto setup = [&](const Loop &loop) -> uvco::Promise<void> {
     Resolver resolver{loop};
-    Promise<AddressHandle> ahPromise = resolver.gai("dns.google", 443, AF_INET);
-    AddressHandle address = co_await ahPromise;
-    EXPECT_EQ(address.port(), 443);
-    EXPECT_TRUE(address.address().starts_with("8.8."));
+    for (const auto af : {AF_INET, AF_INET6}) {
+      Promise<AddressHandle> ahPromise = resolver.gai("dns.google", 443, af);
+      AddressHandle address = co_await ahPromise;
+      EXPECT_EQ(address.port(), 443);
+      // Let's hope Google doesn't change these!
+      EXPECT_TRUE(af != AF_INET || address.address().starts_with("8.8."));
+      EXPECT_TRUE(af != AF_INET6 || address.address().starts_with("2001:4860"));
+    }
   };
 
   run_loop(setup);
